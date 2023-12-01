9a3298cc6e3c5bffa2186e42fd60ec6913458337
Edward Higgins <106348326+kiereshka@users.noreply.github.com>
2023-12-01 11:43:15 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+import Header from "./components/Header.js";
+import Quiz from "./components/Quiz.js";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Quiz />
+      </main>
+    </>
+  );
+}
+
+export default App;
